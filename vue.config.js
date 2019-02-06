5eabd3282ee702520cc91b04ba85efe221829e29
swiesend <sebastian@wiesendahl.de>
2019-02-06 13:00:04 -0600

    add prerendering with prerender-spa-plugin to vue-cli

--- vue.config.js
+++ vue.config.js
@@ -20,7 +20,32 @@ module.exports = {
   parallel: undefined, // default: require('os').cpus().length > 1
 
   css: {
-    modules: true
-  }
+    // see: https://cli.vuejs.org/config/#css-modules
+    modules: true // default: false
+  },
+
+  // see: https://cli.vuejs.org/config/#configurewebpack
+  // see also: https://cli.vuejs.org/guide/webpack.html#simple-configuration
+  // configureWebpack: Function
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      const path = require('path');
+      const PrerenderSPAPlugin = require('prerender-spa-plugin');
+
+      console.log(config.plugins)
+
+      config.plugins.push(
+        new PrerenderSPAPlugin({
+          staticDir: path.join(__dirname, 'dist'), // TODO: reference to outputDir
+          // routes: [ '/', '/about', '/some/deep/nested/route' ],
+          routes: [ '/' ], // TODO infer all routes
+        })
+      )
+    }
+  },
+
+  // see: https://cli.vuejs.org/config/#chainwebpack
+  // see also: https://cli.vuejs.org/guide/css.html#css-modules
+  // chainWebpack: Function
 
 }
