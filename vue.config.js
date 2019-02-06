7af8459bb6a59502258a59d8050fa860d844c9dc
swiesend <sebastian@wiesendahl.de>
2019-02-06 11:58:52 -0600

    add vuex vue-router prerender-spa

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  assetsDir: 'assets',
+  publicPath: '/', // default: '/'
+  outputDir: 'dist', // default: 'dist'
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+
+  css: {
+    modules: true
+  }
+
+}
