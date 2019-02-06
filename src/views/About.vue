44cce687292d043dd58af8b8f8a3763788f46dc3
swiesend <sebastian@wiesendahl.de>
2019-02-06 16:56:43 -0600

    add test images

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <img src="../assets/big.jpg" alt="big jpg">
+    <img src="../assets/big.png" alt="big png">
+    <img src="../assets/big.gif" alt="big gif">
   </div>
 </template>
