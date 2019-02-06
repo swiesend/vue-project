48f41cc470f8997e884bcc8d65fd2ab0a344520c
swiesend <sebastian@wiesendahl.de>
2019-02-06 12:17:00 -0600

    add comments to vue config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,23 @@
 module.exports = {
-  assetsDir: 'assets',
+
+  // see: https://cli.vuejs.org/config/#assetsdir
+  assetsDir: 'assets', // default: ''
+
+  // see: https://cli.vuejs.org/config/#publicpath
   publicPath: '/', // default: '/'
+
+  // see: https://cli.vuejs.org/config/#outputdir
   outputDir: 'dist', // default: 'dist'
+  
+  // see: https://cli.vuejs.org/config/#runtimecompiler
+  // see also: https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
   runtimeCompiler: undefined,
-  productionSourceMap: undefined,
-  parallel: undefined,
+
+  // see: https://cli.vuejs.org/config/#productionsourcemap
+  productionSourceMap: false, // default: true
+
+  // see: https://cli.vuejs.org/config/#parallel
+  parallel: undefined, // default: require('os').cpus().length > 1
 
   css: {
     modules: true
