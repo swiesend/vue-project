142ff7f099b0d9c83071d2c64ef609a66c04e1a4
swiesend <sebastian@wiesendahl.de>
2019-02-06 15:25:08 -0600

    add image optimization

--- vue.config.js
+++ vue.config.js
@@ -24,16 +24,18 @@ module.exports = {
     modules: true // default: false
   },
 
-  // see: https://cli.vuejs.org/config/#configurewebpack
-  // see also: https://cli.vuejs.org/guide/webpack.html#simple-configuration
   // configureWebpack: Function
   configureWebpack: config => {
+
     if (process.env.NODE_ENV === 'production') {
       const path = require('path');
       const PrerenderSPAPlugin = require('prerender-spa-plugin');
 
-      console.log(config.plugins)
-
+      // Prerendering
+      // see: https://cli.vuejs.org/config/#configurewebpack
+      // see also: https://cli.vuejs.org/guide/webpack.html#simple-configuration
+      // see also: https://ssr.vuejs.org/#ssr-vs-prerendering
+      // see also: https://github.com/chrisvfritz/prerender-spa-plugin
       config.plugins.push(
         new PrerenderSPAPlugin({
           staticDir: path.join(__dirname, 'dist'), // TODO: reference to outputDir
@@ -41,11 +43,51 @@ module.exports = {
           routes: [ '/' ], // TODO infer all routes
         })
       )
+
+      // Caching - is enabled by vue-cli by default
+      // see https://webpack.js.org/guides/caching
+      // 1.  https://webpack.js.org/guides/caching/#output-filenames
+      // 2.  https://webpack.js.org/guides/caching/#extracting-boilerplate
+      // 2.1 https://webpack.js.org/plugins/split-chunks-plugin/#splitchunks-chunks
+      // 3.  https://webpack.js.org/guides/caching/#module-identifiers
+
     }
   },
 
   // see: https://cli.vuejs.org/config/#chainwebpack
   // see also: https://cli.vuejs.org/guide/css.html#css-modules
   // chainWebpack: Function
+  chainWebpack: config => {
+    images = config.module.rule('images')
+
+    // add image optimization
+    // see https://github.com/tcoopman/image-webpack-loader
+    images
+      .use('file-loader')
+      .loader('image-webpack-loader')
+      .tap(options => {
+        return {
+          mozjpeg: {
+            quality: 65,
+            progressive: true
+          },
+          optipng: {
+            optimizationLevel: 7, // levels: [0-7]
+            interlaced: false
+          },
+          pngquant:{
+            quality: "65-90",
+            speed: 4
+          },
+          gifsicle: {
+            optimizationLevel: 3, // levels: [1-3]
+            interlaced: false
+          },
+          webp: {
+            quality: 75
+          }
+        }
+      })
+  }
 
 }
